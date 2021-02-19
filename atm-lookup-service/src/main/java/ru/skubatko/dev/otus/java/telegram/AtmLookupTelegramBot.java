2f01a53b16d4b2d74d0bad2b1cb456c1d749f862
Sergey Kubatko <skubatko@gmail.com>
2021-02-19 19:11:07 +0300

    v1.0 / telegram bot initial setup

--- atm-lookup-service/src/main/java/ru/skubatko/dev/otus/java/telegram/AtmLookupTelegramBot.java
+++ atm-lookup-service/src/main/java/ru/skubatko/dev/otus/java/telegram/AtmLookupTelegramBot.java
@@ -1,22 +1,49 @@
 package ru.skubatko.dev.otus.java.telegram;
 
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.skubatko.dev.otus.java.config.TelegramBotProps;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class AtmLookupTelegramBot extends TelegramLongPollingBot {
+
+    private final TelegramBotProps props;
 
     @Override
     public String getBotUsername() {
-        return null;
+        return props.getName();
     }
 
     @Override
     public String getBotToken() {
-        return null;
+        return props.getToken();
     }
 
     @Override
     public void onUpdateReceived(Update update) {
+        log.debug("Receive new Update. updateID: " + update.getUpdateId());
 
+        val chatId = update.getMessage().getChatId().toString();
+        String inputText = update.getMessage().getText();
+
+        if (inputText.startsWith("/start")) {
+            SendMessage message = new SendMessage();
+            message.setChatId(chatId);
+            message.setText("Hello. This is start message");
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
